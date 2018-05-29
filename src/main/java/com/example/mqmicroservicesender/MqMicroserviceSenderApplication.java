34a95e1cbcd7232802f31959f4c31ddf6db47949
Manuel Páez <m.paez589@gmail.com>
2018-05-29 10:34:19 +0200

    Add files via upload

---
+++ src/main/java/com/example/mqmicroservicesender/MqMicroserviceSenderApplication.java
@@ -0,0 +1,12 @@
+package com.example.mqmicroservicesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqMicroserviceSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqMicroserviceSenderApplication.class, args);
+	}
+}
